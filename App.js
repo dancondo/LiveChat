b172230abcd6efd120c6d335ec89452d62830eca
Danilo Raphael Condo <daniloraphaelcondo@gmail.com>
2019-01-03 17:11:21 -0200

    last boileplate commit

--- App.js
+++ App.js
@@ -1,26 +1,10 @@
 import React from 'react';
-import { Container, Header, Content, Card, CardItem, Body, Text } from 'native-base';
+import { Container } from 'native-base';
 
 export default class App extends React.Component {
   render() {
     return (
       <Container>
-        <Header />
-        <Content>
-          <Card>
-            <CardItem bordered>
-              <Body>
-                <Text>
-                  NativeBase is a free and open source framework that enable
-                  developers to build
-                  high-quality mobile apps using React Native iOS and Android
-                  apps
-                  with a fusion of ES6.
-                </Text>
-              </Body>
-            </CardItem>
-          </Card>
-        </Content>
       </Container>
     );
   }
