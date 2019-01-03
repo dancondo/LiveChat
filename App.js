62899d8a98da789e6ee6a642dd064f0bb213cc6e
Danilo Raphael Condo <daniloraphaelcondo@gmail.com>
2019-01-03 17:45:37 -0200

    header

--- App.js
+++ App.js
@@ -1,10 +1,28 @@
 import React from 'react';
 import { Container } from 'native-base';
+import AppHeader from './components/AppHeader';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: true
+    }
+  }
+  async componentWillMount() {
+    await Expo.Font.loadAsync({
+      'Roboto': require('native-base/Fonts/Roboto.ttf'),
+      'Roboto_medium': require('native-base/Fonts/Roboto_medium.ttf'),
+    });
+    this.setState({ loading: false });
+  };
   render() {
+    if (this.state.loading) {
+      return <Container></Container>
+    }
     return (
       <Container>
+        <AppHeader />
       </Container>
     );
   }
